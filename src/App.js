effd8847fde2ee6ee9b23bc2373476b17ea0956c
Uriel <ur13l.infante@gmail.com>
2021-09-30 19:08:39 -0500

    New dev environment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <img src={image} alt="logo" />
-      <h1>Mi primer sitio en Amplify</h1>
+      <h1>Mi primer sitio en Amplify (dev)</h1>
+      <p>Este es un nuevo texto que solo se mostrará en el entorno de Dev</p>
     </div>
   );
 }
